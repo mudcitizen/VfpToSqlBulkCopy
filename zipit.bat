a806a410d43b69dba437d6cf3f8438f17cdb3dce
MudCitizen <mudcitizen@gmail.com>
2018-04-14 17:23:52 -0400

    Misc test stuff

--- zipit.bat
+++ zipit.bat
@@ -1,2 +1,2 @@
-7z a D:\zipz\20180331VfpToSqlBulkCopy2 *.sln *.suo *.cs*  *.sql *.xsd *.config *.testsettings *.resx *.settings *.vdproj .git -r
+7z a D:\zipz\20180412VfpToSqlBulkCopy2 *.sln *.suo *.cs*  *.sql *.xsd *.config *.testsettings *.resx *.settings *.vdproj .git -r
 
